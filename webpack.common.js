e55d9f7459b54737f7fe242537bb6588be5f9272
tamara <tamarachumashvili@gmail.com>
2021-01-02 23:32:29 +0400

    adding video

--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
         use: ["html-loader"]
       },
       {
-        test: /\.(jpg|jpeg|png|gif|svg|pdf)$/,
+        test: /\.(jpg|jpeg|png|gif|svg|pdf|mov|mp4)$/,
         use: [
           {
             loader: "file-loader",
